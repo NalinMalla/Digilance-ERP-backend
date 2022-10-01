c0cbddea569dc374024e57bb5f2dc8925fdfc09b
NalinMalla <nilanallam@gmail.com>
2022-10-01 20:04:55 +0545

    Created userSignInLog

---
+++ models/userSignInLog.model.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const schema = mongoose.Schema;
+
+const UserSignInLogSchema = new schema(
+  {
+    eID: {
+      type: Number,
+      required: [true, "Employee ID is mandatory."],
+    },
+
+    userName: {
+      type: String,
+      required: true,
+      minlength: [5, "User Name should be more than 4 characters."],
+    },
+
+    errorCode: {
+      type: String
+    },
+
+    loggingIp: {
+      type: String,
+      required: [true, "Users IP address is required"]
+    },
+
+    continuityField: {
+      type: String
+    },
+
+    remarks: {
+      type: String
+    }
+ 
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const UserSignInLog = mongoose.model("UserSignInLog", UserSignInLogSchema);
+
+module.exports = UserSignInLog;
